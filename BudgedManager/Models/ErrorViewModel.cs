bd910673cfdc24879de4f4315da459905b65c7a9
Dicenicepl <dicenicepl@gmail.com>
2024-11-01 00:37:22 +0100

    First commit

---
+++ BudgedManager/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BudgedManager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
