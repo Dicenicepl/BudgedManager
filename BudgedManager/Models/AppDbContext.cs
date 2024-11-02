c21629d7e1823466e64c112a02654fb72df9485f
Dicenicepl <dicenicepl@gmail.com>
2024-11-02 16:24:59 +0100

    Creating AppDbContext.cs

---
+++ BudgedManager/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BudgedManager.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgedManager.Models;
+
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options): base (options)
+    {
+            
+    }
+    public DbSet<Expense> Expenses { get; set; }
+    public DbSet<Category> Categories { get; set; }
+}
