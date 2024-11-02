ab25eb46ac8d159603115f5424ce6e4bfb017b76
Dicenicepl <dicenicepl@gmail.com>
2024-11-02 16:25:20 +0100

    Setting up Database context

--- BudgedManager/Program.cs
+++ BudgedManager/Program.cs
@@ -1,8 +1,12 @@
+using BudgedManager.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Secured")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
