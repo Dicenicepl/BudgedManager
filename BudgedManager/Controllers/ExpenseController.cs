87c880c4cfba65a75c1faed22fa6f3ad7414bfec
Dicenicepl <dicenicepl@gmail.com>
2024-11-10 05:24:52 +0100

    Expense end-points with the .cshtml

---
+++ BudgedManager/Controllers/ExpenseController.cs
@@ -0,0 +1,169 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BudgedManager.Models;
+using BudgedManager.Models.Entity;
+
+namespace BudgedManager.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ExpenseController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Expense
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Expenses.ToListAsync());
+        }
+
+        // GET: Expense/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expense = await _context.Expenses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            return View(expense);
+        }
+
+        // GET: Expense/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        // todo: configure functionality for below end-points
+        //GET: Expense/Calculator
+        public IActionResult Calculate()
+        {
+            return View();
+        }
+        
+        //GET: Expense/History
+        public IActionResult History()
+        {
+            return View();
+        }
+
+        // POST: Expense/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Amount,Date")] Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(expense);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(expense);
+        }
+
+        // GET: Expense/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expense = await _context.Expenses.FindAsync(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+            return View(expense);
+        }
+
+        // POST: Expense/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Amount,Date")] Expense expense)
+        {
+            if (id != expense.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(expense);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExpenseExists(expense.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(expense);
+        }
+
+        // GET: Expense/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expense = await _context.Expenses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            return View(expense);
+        }
+
+        // POST: Expense/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var expense = await _context.Expenses.FindAsync(id);
+            if (expense != null)
+            {
+                _context.Expenses.Remove(expense);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ExpenseExists(int id)
+        {
+            return _context.Expenses.Any(e => e.Id == id);
+        }
+    }
+}
