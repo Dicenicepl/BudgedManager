174c27d7a608c822f2ce1c4ed145355487ab84a0
Dicenicepl <dicenicepl@gmail.com>
2025-01-27 04:10:48 +0100

    Git: commit folder Views & Controllers

---
+++ BudgedManager/Controllers/SubscriptionController.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BudgedManager.Models;
+using BudgedManager.Models.Entity;
+
+namespace BudgedManager.Controllers
+{
+    public class SubscriptionController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SubscriptionController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Subscription
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Subscriptions.ToListAsync());
+        }
+
+        // GET: Subscription/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subscription = await _context.Subscriptions
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (subscription == null)
+            {
+                return NotFound();
+            }
+
+            return View(subscription);
+        }
+
+        // GET: Subscription/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Subscription/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,subscriptionName,subscriptionDescription,subscriptionStartDate,subscriptionPaymentPeriod,subsriptionPrice")] Subscription subscription)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(subscription);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(subscription);
+        }
+
+        // GET: Subscription/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subscription = await _context.Subscriptions.FindAsync(id);
+            if (subscription == null)
+            {
+                return NotFound();
+            }
+            return View(subscription);
+        }
+
+        // POST: Subscription/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,subscriptionName,subscriptionDescription,subscriptionStartDate,subscriptionPaymentPeriod,subsriptionPrice")] Subscription subscription)
+        {
+            if (id != subscription.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(subscription);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SubscriptionExists(subscription.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(subscription);
+        }
+
+        // GET: Subscription/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subscription = await _context.Subscriptions
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (subscription == null)
+            {
+                return NotFound();
+            }
+
+            return View(subscription);
+        }
+
+        // POST: Subscription/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var subscription = await _context.Subscriptions.FindAsync(id);
+            if (subscription != null)
+            {
+                _context.Subscriptions.Remove(subscription);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SubscriptionExists(int id)
+        {
+            return _context.Subscriptions.Any(e => e.id == id);
+        }
+    }
+}
