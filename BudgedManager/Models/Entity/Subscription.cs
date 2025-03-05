2a95bb68e33c983eeaaf71fd6368447920ba8ea9
Dicenicepl <dicenicepl@gmail.com>
2025-03-05 03:09:48 +0100

    Add requirement to create Subscription.cs

--- BudgedManager/Models/Entity/Subscription.cs
+++ BudgedManager/Models/Entity/Subscription.cs
@@ -5,11 +5,19 @@ namespace BudgedManager.Models.Entity;
 public class Subscription
 {
     public int Id { get; set; }
+    [Required (AllowEmptyStrings = false)]
+    [MinLength (2)]
+    [MaxLength(20)]
     public string SubscriptionName { get; set; }
+    
+    [Required (AllowEmptyStrings = true)]
+    [MaxLength(240)]
     public string SubscriptionDescription { get; set; }
+    
+    
     public DateTime SubscriptionStartDate { get; set; }
     [Range(1, int.MaxValue)]
     public int SubscriptionPaymentPeriod { get; set; }
-    // [Range(1, int.MaxValue)]
+    [Range(1, int.MaxValue)]
     public decimal SubscriptionPrice { get; set; }
 }
