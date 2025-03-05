0f16e0a6de5e4969daa0e8a025235ee35461627c
Dicenicepl <dicenicepl@gmail.com>
2025-03-05 03:09:14 +0100

    Clearing PDFWrapper.cs

--- BudgedManager/Models/Entity/PDFWrapper.cs
+++ BudgedManager/Models/Entity/PDFWrapper.cs
@@ -5,20 +5,5 @@ namespace BudgedManager.Models.Entity;
 
 public class PDFWrapper
 {
-    private readonly AppDbContext _context;
     
-    public PDFWrapper(AppDbContext context)
-    {
-        this._context = context;
-    }
-
-    public void Import()
-    {
-        
-    }
-
-    void export()
-    {
-        
-    }
 }
