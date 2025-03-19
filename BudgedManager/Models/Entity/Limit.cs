d36172b1ece02559f1dd1d0c003eab268bbb6178
Dicenicepl <dicenicepl@gmail.com>
2025-03-19 18:20:58 +0100

    Creating range for the int values of Amount data

--- BudgedManager/Models/Entity/Limit.cs
+++ BudgedManager/Models/Entity/Limit.cs
@@ -1,9 +1,15 @@
-﻿namespace BudgedManager.Models.Entity;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BudgedManager.Models.Entity;
 
 public class Limit
 {
     public int Id { get; set; }
     public int CategoryId { get; set; }
+    
+    [Range(0, int.MaxValue)]
     public int LimitWarning { get; set; } //set LimitAlert = Limit * 0.9 for 90% of Limit
+    
+    [Range(0, int.MaxValue)]
     public int LimitAlert { get; set; }
 }
