47d8d20f9c9159236f10835cd479b647f726afa3
Dicenicepl <dicenicepl@gmail.com>
2025-04-28 18:14:26 +0200

    Test of Import data

--- BudgedManager/Services/Import.cs
+++ BudgedManager/Services/Import.cs
@@ -1,6 +1,45 @@
-﻿namespace BudgedManager.Services;
+﻿using Humanizer;
+
+namespace BudgedManager.Services;
 
 public class Import
 {
-    
+    private string _path;
+    public void Start(string type, string path)
+    {
+        _path = path;
+        switch (type)
+        {
+            case "json": JsonFormat();
+                break;
+            case "txt": TxtFormat();
+                break;
+            case "xml": XmlFormat();
+                break;
+            default: Console.Error.WriteLine("Unknown export type: " + type);
+                break;
+        }
+        Console.WriteLine("Import complete.");
+    }
+
+    private void JsonFormat()
+    {
+        var test = File.ReadAllLines(_path);
+        foreach (var VARIABLE in test)
+        {
+            if (VARIABLE.IndexOf(':') > 0)
+            {
+                Console.WriteLine(VARIABLE.Substring(VARIABLE.IndexOf(':') + 2).Replace(",", ""));
+            }
+            
+        }
+    }
+    private void TxtFormat()
+    {
+        
+    }
+    private void XmlFormat()
+    {
+        
+    }
 }
