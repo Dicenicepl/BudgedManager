9331227cfef8917fbdf9340559189a842194aa46
Dicenice <dicenicepl@gmail.com>
2025-07-31 20:55:56 +0200

    Download method tested succesfully

--- BudgedManager/Controllers/TestController.cs
+++ BudgedManager/Controllers/TestController.cs
@@ -5,46 +5,16 @@ using Microsoft.AspNetCore.Mvc;
 
 public class TestController : Controller
 {
-
-    public void Index()
+    public IActionResult Index()
     {
-        var serializer = new XmlSerializer(typeof(ExpenseList));
-        ExpenseList expenseList = new ExpenseList();
-        expenseList.Items = new List<Expense>();
+        return Content(@"<html><body><a href=""http://localhost:5062/Test/download/"" download>test</a></body></html>", "text/html");
 
-        Expense expense1 = new Expense();
-        expense1.Amount = 11;
-        expense1.CategoryId = 1;
-        expense1.Date = DateTime.Now;
-        expense1.Comment = "expense1";
-
-        Expense expense2 = new Expense();
-        expense2.Id = 1;
-        expense2.Amount = 12;
-        expense2.CategoryId = 1;
-        expense2.Date = DateTime.Now;
-        expense2.Comment = "expense2";
-
-        Expense expense3 = new Expense();
-        expense3.Amount = 13;
-        expense3.CategoryId = 1;
-        expense3.Date = DateTime.Now;
-        expense3.Comment = "expense3";
-
-        Expense expense4 = new Expense();
-        expense4.Amount = 14;
-        expense4.CategoryId = 1;
-        expense4.Date = DateTime.Now;
-        expense4.Comment = "expense4";
-
-        expenseList.Items.Add(expense1);
-        expenseList.Items.Add(expense2);
-        expenseList.Items.Add(expense3);
-        expenseList.Items.Add(expense4);
-
-
-        serializer.Serialize(Console.Out, expenseList);
-        Console.WriteLine();
-        Console.ReadLine();
     }
+    public IActionResult Download()
+    {
+        var content = System.IO.File.ReadAllBytes("C:\\Users\\Dicenice\\RiderProjects\\BudgedManager\\BudgedManager\\wwwroot\\files\\export.xml");
+        return File(content, "application/xml", "export.xml");
+    }
+
+
 }
