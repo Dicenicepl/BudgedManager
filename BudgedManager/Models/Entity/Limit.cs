f8121163bb3a13790bcab9517aad2eacec020b42
Dicenice <dicenicepl@gmail.com>
2025-08-05 21:21:13 +0200

    Entity Limit create Object Categoy for easier access to Name

--- BudgedManager/Models/Entity/Limit.cs
+++ BudgedManager/Models/Entity/Limit.cs
@@ -6,6 +6,7 @@ public class Limit
 {
     public int Id { get; set; }
     public int CategoryId { get; set; }
+    public Category? Category { get;  set;}
     
     [Range(0, int.MaxValue)]
     public decimal LimitWarning { get; set; } //set LimitAlert = Limit * 0.9 for 90% of Limit
